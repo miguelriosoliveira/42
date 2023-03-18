d30ac90a0693e7ce5bc211d42ed009ecf64a2f5f
Miguel Rios <miguelriosoliveira@gmail.com>
2023-03-18 01:36:22 +0100

    question: c00/ex06

--- c/c00/ex06/ft_print_comb2.c
+++ c/c00/ex06/ft_print_comb2.c
@@ -1,2 +1,35 @@
 #include <unistd.h>
 
+void ft_putchar(char c) {
+	write(1, &c, 1);
+}
+
+void ft_print_comb2(void) {
+	int a = 0;
+	while (a < 99) {
+		int b = a + 1;
+		while (b < 100) {
+			int a_integer_part = a / 10;
+			int a_remainder = a % 10;
+			int b_integer_part = b / 10;
+			int b_remainder = b % 10;
+			ft_putchar('0' + a_integer_part);
+			ft_putchar('0' + a_remainder);
+			ft_putchar(' ');
+			ft_putchar('0' + b_integer_part);
+			ft_putchar('0' + b_remainder);
+			if (a != 98) {
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int main(void) {
+	ft_print_comb2();
+	return 0;
+}
+
