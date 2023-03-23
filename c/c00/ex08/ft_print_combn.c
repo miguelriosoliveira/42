13624334c550031f61903d2d43ce6708627b1c05
Miguel Rios <miguelriosoliveira@gmail.com>
2023-03-23 01:53:14 +0100

    chore: ignoring vim files

--- c/c00/ex08/ft_print_combn.c
+++ c/c00/ex08/ft_print_combn.c
@@ -6,23 +6,13 @@ void ft_putchar(char c) {
 	write(1, &c, 1);
 }
 
-void print_combn(int n, int start, char* buffer) {
-	if (n == 0) {
-		write(1, buffer, MAX_N);
+void ft_print_combn(int n, int start) {
+	int a = '0' + start;
+	int end = '9' - n + 1;
+	while (a <= end) {
+		ft_print_combn(n-1, start+1);
+		a++;
 	}
-
-	int i = start;
-	while (i <= MAX_N) {
-		char new_buffer[MAX_N] = buffer + '0' + i;
-		print_combn(n-1, i+1, new_buffer);
-		i++;
-	}
-}
-
-void ft_print_combn(int n) {
-	int start = 0;
-	char* buffer = "";
-	print_combn(n, start, buffer);
 }
 
 int main(void) {
