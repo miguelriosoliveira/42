48f064a4d6073b2be1a453f26755d1937fb1e301
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-01 16:40:41 +0200

    Update ft_is_negative.c

--- c/c00/ex04/ft_is_negative.c
+++ c/c00/ex04/ft_is_negative.c
@@ -1,14 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 16:29:35 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/01 16:40:06 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
-  write(1, &c, 1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void ft_is_negative(int n) {
-  char result = n < 0 ? 'N' : 'P';
-  ft_putchar(result);
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
+	}
 }
 
+/*
 int main(void) {
   ft_is_negative(-42);
   ft_putchar('\n');
@@ -16,3 +37,4 @@ int main(void) {
   ft_putchar('\n');
   return 0;
 }
+*/
