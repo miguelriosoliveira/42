4323be39662feb16fb9b7c13bd6e9398a4171d2d
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-01 15:30:15 +0200

    Update ft_print_alphabet.c

--- c/c00/ex01/ft_print_alphabet.c
+++ c/c00/ex01/ft_print_alphabet.c
@@ -1,18 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 15:17:28 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/01 15:26:55 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
-  write(1, &c, 1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void ft_print_alphabet(void) {
-  int i = 'a';
-  while (i <= 'z') {
-    ft_putchar(i);
-    i++;
-  }
+void	ft_print_alphabet(void)
+{
+	int	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		ft_putchar(i);
+		i++;
+	}
 }
 
+/*
 int main(void) {
   ft_print_alphabet();
   return 0;
 }
+*/
