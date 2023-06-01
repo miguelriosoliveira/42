5aad13f215c6782ba2b26530c8b244f5b3875c3f
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-01 16:07:33 +0200

    Update ft_print_numbers.c

--- c/c00/ex03/ft_print_numbers.c
+++ c/c00/ex03/ft_print_numbers.c
@@ -1,18 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 15:39:50 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/01 15:44:04 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
-  write(1, &c, 1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void ft_print_numbers(void) {
-  int i = '0';
-  while (i <= '9') {
-    ft_putchar(i);
-    i++;
-  }
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		ft_putchar(i);
+		i++;
+	}
 }
 
+/*
 int main(void) {
   ft_print_numbers();
   return 0;
 }
+*/
