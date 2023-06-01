7033f4a65ca6cc91b06a624de576a6589c6f7de8
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-01 18:51:45 +0200

    Update ft_print_comb.c

--- c/c00/ex05/ft_print_comb.c
+++ c/c00/ex05/ft_print_comb.c
@@ -1,32 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 16:43:05 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/01 18:51:09 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void	ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-void ft_print_comb(void) {
-  int i = 0;
-  while (i <= 7) {
-    int j = i+1;
-    while (j <= 8) {
-      int k = j+1;
-      while (k <= 9) {
-        ft_putchar('0' + i);
-        ft_putchar('0' + j);
-        ft_putchar('0' + k);
-        if (i < 7) {
-        	ft_putchar(',');
-        	ft_putchar(' ');
-        }
-        k++;
-      }
-      j++;
-    }
-    i++;
-  }
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_putchar('0' + i);
+				ft_putchar('0' + j);
+				ft_putchar('0' + k);
+				if (i < 7)
+					write(1, ", ", 2);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
+/*
 int main(void) {
   ft_print_comb();
   return 0;
 }
+*/
