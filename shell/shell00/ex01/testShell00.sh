528d94b500ae0283e4a8155166974a36edc84bcd
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-01 14:48:00 +0200

    Update testShell00.sh

--- shell/shell00/ex01/testShell00.sh
+++ shell/shell00/ex01/testShell00.sh
@@ -1,5 +1,5 @@
 printf %40s | tr " " "=" > testShell00
-touch -d "2022-06-01T23:42:00" testShell00
+touch -t 202206012342 testShell00
 chmod 455 testShell00
 ls -l
 tar -cf testShell00.tar testShell00
