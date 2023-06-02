6f1e9c0b39b6057014c9c7afed3ade8763a1d835
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-02 14:49:15 +0200

    Update ft_putstr.c

--- c/c01/ex05/ft_putstr.c
+++ c/c01/ex05/ft_putstr.c
@@ -1,21 +1,40 @@
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 14:46:03 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/02 14:46:05 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putchar(char c) {
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str) {
-	int i = 0;
-	while (str[i]) {
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
 		ft_putchar(str[i]);
 		i++;
 	}
 }
+
+/*
+#include <stdio.h>
 
 int main(void) {
 	ft_putstr("patatas bravas!");
 
 	return 0;
 }
-
+*/
