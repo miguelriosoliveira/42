f0259667b13267b3746b2ca355a1a3fadc5bd459
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-02 14:41:38 +0200

    Update ft_div_mod.c

--- c/c01/ex03/ft_div_mod.c
+++ c/c01/ex03/ft_div_mod.c
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 14:40:00 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/02 14:40:01 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-void ft_div_mod(int a, int b, int *div, int *mod) {
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
 	*div = a / b;
 	*mod = a % b;
 }
 
+/*
 int main(void) {
 	int a = 7;
 	int b = 3;
@@ -17,4 +31,4 @@ int main(void) {
 	
 	return 0;
 }
-
+*/
