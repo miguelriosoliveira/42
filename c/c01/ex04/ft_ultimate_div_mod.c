62f697890a50d2938fca5d58fd07149cc9c10cab
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-02 14:45:32 +0200

    Update ft_ultimate_div_mod.c

--- c/c01/ex04/ft_ultimate_div_mod.c
+++ c/c01/ex04/ft_ultimate_div_mod.c
@@ -1,12 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 14:43:33 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/02 14:43:37 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-void ft_ultimate_div_mod(int *a, int *b) {
-	int div = *a / *b;
-	int mod = *a % *b;
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
 	*a = div;
 	*b = mod;
 }
 
+/*
 int main(void) {
 	int a = 7;
 	int b = 3;
@@ -17,4 +34,4 @@ int main(void) {
 
 	return 0;
 }
-
+*/
