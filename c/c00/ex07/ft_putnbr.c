9332ef29f118436fc7785e8b709809f7abcfe978
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-02 13:32:06 +0200

    Update ft_putnbr.c

--- c/c00/ex07/ft_putnbr.c
+++ c/c00/ex07/ft_putnbr.c
@@ -1,22 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 19:08:18 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/01 19:23:19 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void	ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb) {
-	if (nb < 0) {
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	
-	if (nb >= 10) {
+	if (nb >= 10)
+	{
 		ft_putnbr(nb / 10);
 	}
-
 	ft_putchar('0' + nb % 10);
 }
 
+/*
 int main(void) {
 	ft_putnbr(2);
 	ft_putchar('\n');
@@ -31,4 +46,4 @@ int main(void) {
 	ft_putchar('\n');
 	return 0;
 }
-
+*/
