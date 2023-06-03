7ebf3b21161779d2d5ef2c1abd4d78a9971bbfa3
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-03 17:09:36 +0200

    Update ft_sort_int_tab.c

--- c/c01/ex08/ft_sort_int_tab.c
+++ c/c01/ex08/ft_sort_int_tab.c
@@ -1,3 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 14:56:37 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/02 14:56:39 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	swap_count;
+	int	i;
+
+	swap_count = 1;
+	while (swap_count > 0)
+	{
+		swap_count = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				ft_swap(&tab[i], &tab[i + 1]);
+				swap_count++;
+			}
+			i++;
+		}
+	}
+}
+
+/*
 #include <stdio.h>
 
 void print_array(int* array, int size) {
@@ -13,27 +57,6 @@ void print_array(int* array, int size) {
 	}
 
 	printf("]");
-}
-
-void ft_swap(int* a, int* b) {
-	int aux = *a;
-	*a = *b;
-	*b = aux;
-}
-
-void ft_sort_int_tab(int *tab, int size) {
-	int swap_count;
-	do {
-		swap_count = 0;
-		int i = 0;
-		while (i < size-1) {
-			if (tab[i] > tab[i+1]) {
-				ft_swap(&tab[i], &tab[i+1]);
-				swap_count++;
-			}
-			i++;
-		}
-	} while (swap_count > 0);
 }
 
 int main(void) {
@@ -52,4 +75,4 @@ int main(void) {
 
 	return 0;
 }
-
+*/
