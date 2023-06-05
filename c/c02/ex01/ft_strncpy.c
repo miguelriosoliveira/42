36026bdd1967f0a4d855aa6b01f8f0da2f3cf00b
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-05 11:14:36 +0200

    Update ft_strncpy.c

--- c/c02/ex01/ft_strncpy.c
+++ c/c02/ex01/ft_strncpy.c
@@ -1,52 +1,61 @@
-char *ft_strncpy(char *dest, char *src, unsigned int n) {
-	unsigned int i = 0;
-	while (i < n) {
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 09:44:42 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/05 09:44:44 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
 
+/*
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
-	char src[]  = "source";
-	char dest[7];
-	
-	printf("#ft_strncpy\n");
-	printf("\n");
-	printf("Antes:\n");
-	printf("src:  \"%s\"\n", src);
-	printf("dest: \"%s\"\n", dest);
-	
-	ft_strncpy(dest, src, 4);
+	char dst1[] = "World1";
+	char src1[] = "Hello";
+	int n1 = 3;
+	char *strncpy_res1 = strncpy(dst1, src1, n1);
+	char *ft_strncpy_res1 = ft_strncpy(dst1, src1, n1);
+	printf("   strncpy \"%s\" \"%s\" %d: %s\n", dst1, src1, n1,    strncpy_res1);
+	printf("ft_strncpy \"%s\" \"%s\" %d: %s\n", dst1, src1, n1, ft_strncpy_res1);
 	
 	printf("\n");
-	printf("Luego:\n");
-	printf("src:  \"%s\"\n", src);
-	printf("dest: \"%s\"\n", dest);
-	
-	// ----------------------------
-	
-	char src2[]  = "source";
-	char dest2[7];
-	
-	printf("\n");
-	printf("#strncpy\n");
-	printf("\n");
-	printf("Antes:\n");
-	printf("src2:  \"%s\"\n", src2);
-	printf("dest2: \"%s\"\n", dest2);
 
-	strncpy(dest2, src2, 4);
-	
-	printf("\n");
-	printf("Luego:\n");
-	printf("src2:  \"%s\"\n", src2);
-	printf("dest2: \"%s\"\n", dest2);
+	char dst2[] = "World1";
+	char src2[] = "Hello";
+	int n2 = 5;
+	char *strncpy_res2 = strncpy(dst2, src2, n2);
+	char *ft_strncpy_res2 = ft_strncpy(dst2, src2, n2);
+	printf("   strncpy \"%s\" \"%s\" %d: %s\n", dst2, src2, n2,    strncpy_res2);
+	printf("ft_strncpy \"%s\" \"%s\" %d: %s\n", dst2, src2, n2, ft_strncpy_res2);
 
+	printf("\n");
+	
+	char dst3[] = "World1";
+	char src3[] = "Hello";
+	int n3 = 7;
+	char *strncpy_res3 = strncpy(dst3, src3, n3);
+	char *ft_strncpy_res3 = ft_strncpy(dst3, src3, n3);
+	printf("   strncpy \"%s\" \"%s\" %d: %s\n", dst3, src3, n3,    strncpy_res3);
+	printf("ft_strncpy \"%s\" \"%s\" %d: %s\n", dst3, src3, n3, ft_strncpy_res3);
+	 
 	return 0;
 }
-
+*/
