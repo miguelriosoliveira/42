6bb11fea0e558b3db4b4e2e7b4975e2f48540e46
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-05 12:04:18 +0200

    Update ft_str_is_numeric.c

--- c/c02/ex03/ft_str_is_numeric.c
+++ c/c02/ex03/ft_str_is_numeric.c
@@ -1,16 +1,30 @@
-int ft_str_is_numeric(char *str) {
-	int i = 0;
-	while (str[i]) {
-		int is_numeric = str[i] >= '0' && str[i] <= '9';
-		if (!is_numeric) {
-			return 0;
-		}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 12:02:16 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/05 12:02:21 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
 		i++;
 	}
-
-	return 1;
+	return (1);
 }
 
+/*
 #include <stdio.h>
 
 int main(void) {
@@ -28,3 +42,4 @@ int main(void) {
 
 	return 0;
 }
+*/
