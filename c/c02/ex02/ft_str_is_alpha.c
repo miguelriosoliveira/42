cf9a7bde6e8addfd183fc00fef48217289649124
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-05 12:00:46 +0200

    Update ft_str_is_alpha.c

--- c/c02/ex02/ft_str_is_alpha.c
+++ c/c02/ex02/ft_str_is_alpha.c
@@ -1,34 +1,54 @@
-int ft_str_is_alpha(char *str) {
-	int i = 0;
-	while (str[i]) {
-		int is_upper_case = str[i] >= 'A' && str[i] <= 'Z';
-		int is_lower_case = str[i] >= 'a' && str[i] <= 'z';
-		if (!is_upper_case && !is_lower_case) {
-			return 0;
-		}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 11:16:33 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/05 11:16:36 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z')
+			&& !(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
+/*
 #include <stdio.h>
 
 int main(void) {
 	char* alpha_word = "yes";
-	printf("\"%s\" is alpha? %d\n", alpha_word, ft_str_is_alpha(alpha_word));
+	char *result1 = ft_str_is_alpha(alpha_word);
+	printf("\"%s\" is alpha? %d\n", alpha_word, result1);
 	
 	char* not_alpha_word = "yes1";
-	printf("\"%s\" is alpha? %d\n", not_alpha_word, ft_str_is_alpha(not_alpha_word));
+	char *result2 = ft_str_is_alpha(not_alpha_word);
+	printf("\"%s\" is alpha? %d\n", not_alpha_word, result2);
 	
 	char* empty_word = "";
-	printf("\"%s\" is alpha? %d\n", empty_word, ft_str_is_alpha(empty_word));
+	char *result3 = ft_str_is_alpha(empty_word);
+	printf("\"%s\" is alpha? %d\n", empty_word, result3);
 
 	char* uppercase_alpha_word = "POTATO";
-	printf("\"%s\" is alpha? %d\n", uppercase_alpha_word, ft_str_is_alpha(uppercase_alpha_word));
+	char *result3 = ft_str_is_alpha(uppercase_alpha_word);
+	printf("\"%s\" is alpha? %d\n", uppercase_alpha_word, result3);
 	
 	char* uppercase_not_alpha_word = "P07470";
-	printf("\"%s\" is alpha? %d\n", uppercase_not_alpha_word, ft_str_is_alpha(uppercase_not_alpha_word));
+	char *result3 = ft_str_is_alpha(uppercase_not_alpha_word);
+	printf("\"%s\" is alpha? %d\n", uppercase_not_alpha_word, result3);
 	
 	return 0;
 }
-
+*/
