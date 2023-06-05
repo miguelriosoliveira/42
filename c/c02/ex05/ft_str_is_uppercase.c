dfba3eaa8f4f725218a4ff20a9f77e1f9344b924
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-05 12:08:34 +0200

    Update ft_str_is_uppercase.c

--- c/c02/ex05/ft_str_is_uppercase.c
+++ c/c02/ex05/ft_str_is_uppercase.c
@@ -1,16 +1,30 @@
-int ft_str_is_uppercase(char *str) {
-	int i = 0;
-	while (str[i]) {
-		int is_uppercase = str[i] >= 'A' && str[i] <= 'Z';
-		if (!is_uppercase) {
-			return 0;
-		}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 12:07:10 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/05 12:07:12 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z'))
+			return (0);
 		i++;
 	}
-
-	return 1;
+	return (1);
 }
 
+/*
 #include <stdio.h>
 
 int main(void) {
@@ -28,4 +42,4 @@ int main(void) {
 
 	return 0;
 }
-
+*/
