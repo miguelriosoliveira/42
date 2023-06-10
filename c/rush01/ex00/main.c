9b9ff334e3b1935bae7d88388cfb9fbf6c93037c
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-10 19:57:07 +0200

    Add files via upload

---
+++ c/rush01/ex00/main.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 13:43:56 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/10 13:44:00 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SIDE 4
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_error(void)
+{
+	write(1, "Error\n", 6);
+}
+
+void	print_map(int **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < SIDE)
+	{
+		ft_putchar(map[i][1] + '0');
+		j = 1;
+		while (j < SIDE)
+		{
+			ft_putchar(' ');
+			ft_putchar(map[i][j] + '0');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int	**create_map(void)
+{
+	int	i;
+	int	j;
+	int	**map;
+
+	i = 0;
+	map = malloc(SIDE * sizeof(int *));
+	while (i < SIDE)
+	{
+		map[i] = malloc(SIDE * sizeof(int));
+		i++;
+	}
+	while (i < SIDE)
+	{
+		j = 0;
+		while (j < SIDE)
+		{
+			map[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (map);
+}
+
+void	free_map(int **map)
+{
+	int	i;
+
+	i = 0;
+	while (i < SIDE)
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+// void	solve_puzzle(int **map, char *hints)
+// {
+	
+// }
+
+int	main(int argc, char **argv)
+{
+	char	*input;
+	int		**map;
+
+	if (argc != 2)
+	{
+		print_error();
+		return (0);
+	}
+	input = argv[1];
+	// if (ft_strlen(input) != SIDE * SIDE)
+	// {
+	// 	print_error();
+	// 	return (0);
+	// }
+	map = create_map();
+	// solve_puzzle(map, input);
+	print_map(map);
+	free_map(map);
+	return (0);
+}
