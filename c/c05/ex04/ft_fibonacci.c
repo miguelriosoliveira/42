6f107c78d28b73c15ad7dec10bb4041fa693f126
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-16 14:49:46 +0200

    Update ft_fibonacci.c

--- c/c05/ex04/ft_fibonacci.c
+++ c/c05/ex04/ft_fibonacci.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrios-es <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 14:42:33 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/06/16 14:42:35 by mrios-es         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
@@ -6,3 +18,15 @@ int	ft_fibonacci(int index)
 		return (index);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
+
+/*
+#include <stdio.h>
+
+int main() {
+	int index = -2;
+	while (index <= 10) {
+		printf("ft_fibonacci(%2d): %d\n", index, ft_fibonacci(index));
+		index++;
+	}
+}
+*/
