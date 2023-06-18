8084a4502999c256c3874c1bbd4fe967643e24d8
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-19 00:07:05 +0200

    c05ex05

--- c/c05/ex05/ft_sqrt.c
+++ c/c05/ex05/ft_sqrt.c
@@ -1,4 +1,31 @@
 int	ft_sqrt(int nb)
 {
+  long result;
 
+  if (nb == 1)
+    return (1);
+  result = nb / 2;
+  while (result > 0 && result * result > nb)
+  {
+    result += nb / result;
+    result /= 2;
+  }
+  if (result * result == nb)
+    return (result);
+  return (0);
 }
+
+/*
+#include <stdio.h>
+int main() {
+  int nb = 0;
+  int result = 0;
+  while (nb < 1000) {
+    result = ft_sqrt(nb);
+    if (result)
+      printf("ft_sqrt(%3d): %d\n", nb, result);
+    nb++;
+  }
+  printf("%d\n", ft_sqrt(4000000));
+}
+*/
