84eb3016111ba139e4c9590e369568c3cec825cf
Miguel Rios <miguelriosoliveira@gmail.com>
2023-06-19 10:21:29 +0200

    wip: c05ex08

--- c/c05/ex08/ft_ten_queens_puzzle.c
+++ c/c05/ex08/ft_ten_queens_puzzle.c
@@ -1,17 +1,23 @@
+int	is_valid(int *queens_positions, int step)
+{
+
+}
+
 int	solve(int *queens_positions, int step, int *solution_count)
 {
 	if (step > 10)
 		return *solution_count;
-	while (queens_positions[step] != 10 && *solution_count == 0)
+	while (queens_positions[step] != 10 && *solution_count != 10)
 	{
 		queens_positions[step]++;
 		if (is_valid(queens_positions, step)) {
 			if (step != 10)
-				*solution_count = solve(queens_positions, step + 1);
+				*solution_count = solve(queens_positions, step + 1, solution_count);
 			else
 				*solution_count += 1;
 		}
 	}
+	return *solution_count;
 }
 
 int	ft_ten_queens_puzzle(void)
