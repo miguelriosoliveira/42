94ae0fa880f822712c28a1852e6d7cde28a213b7
Miguel Rios <miguelriosoliveira@gmail.com>
2023-08-10 23:45:02 +0200

    fix: Norminette on c08ex00

--- c/c08/ex01/ft_boolean.h
+++ c/c08/ex01/ft_boolean.h
@@ -1,22 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrios-es <mrios-es@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/10 23:41:13 by mrios-es          #+#    #+#             */
-/*   Updated: 2023/08/10 23:41:17 by mrios-es         ###   ########.fr       */
+/*   Created: 2023/08/10 23:44:22 by mrios-es          #+#    #+#             */
+/*   Updated: 2023/08/10 23:44:24 by mrios-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef FT_H
-# define FT_H
-
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-
-#endif
