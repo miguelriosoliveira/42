944186022eabcfb6577d3dfa5cdbdd545ac67a51
Miguel Rios <miguelriosoliveira@gmail.com>
2023-08-11 14:51:14 +0200

    fix: Norminette on c08ex01

--- c/c08/ex01/ft_boolean.h
+++ c/c08/ex01/ft_boolean.h
@@ -10,16 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN
-# define FT_BOOLEAN
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
-#include <unistd.h>
+# include <unistd.h>
+# define EVEN(n) (n % 2 == 0)
 # define TRUE 1
 # define FALSE 0
-# define SUCCESS 0
-# define EVEN(n) (n % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
 
 typedef int	t_bool;
 
