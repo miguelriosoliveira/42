2e5e42a06ba628b8e501c5a6b75f273ed0a20b24
Miguel Rios <miguelriosoliveira@gmail.com>
2023-09-19 20:50:28 +0200

    feature: `ft_memcmp`

--- cursus/00-libft/ft_memcmp.c
+++ cursus/00-libft/ft_memcmp.c
@@ -12,4 +12,26 @@
 
 #include "libft.h"
 
-int	memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	c1;
+	unsigned char	c2;
+	const char		*str1;
+	const char		*str2;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	str1 = s1;
+	str2 = s2;
+	c1 = str1[0];
+	c2 = str2[0];
+	while (i < n && c1 == c2)
+	{
+		c1 = str1[i];
+		c2 = str2[i];
+		i++;
+	}
+	return (c1 - c2);
+}
