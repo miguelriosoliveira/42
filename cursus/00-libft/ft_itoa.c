48b4d6298ad1e3c52c441c0136c678feae21deaa
Miguel Rios <mrios-es@c1r9s4.42madrid.com>
2023-09-22 21:41:22 +0200

    wip: `ft_itoa`

--- cursus/00-libft/ft_itoa.c
+++ cursus/00-libft/ft_itoa.c
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_itoa(int n);
+#include "libft.h"
+
+#include <stdio.h>
+
+int	count_chars(int nb)
+{
+	long	nbr;
+	int		count;
+
+	nbr = nb;
+	count = 1;
+
+	printf("nbr: %ld\n", nbr);
+
+	if (nbr < 0)
+	{
+		printf("is negative!\n");
+		printf("nbr: %ld\n", nbr);
+		count++;
+		nbr = -nbr;
+	}
+	while (nbr >= 10)
+	{
+		printf("nbr: %ld\n", nbr);
+		count++;
+		nbr /= 10;
+	}
+
+		printf("nbr: %ld\n", nbr);
+
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	printf("char_count: %d\n", count_chars(n));
+	return "";
+}
---
+++ cursus/00-libft/test/ft_itoa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "../libft.h"
+
+int main() {
+	{
+		int n = -123;
+
+		char *expected = "-123";
+		char *received = ft_itoa(n);
+
+		int passed = strcmp(expected, received) == 0;
+		if (!passed) {
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_itoa(%d)\n", result, n);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+
+	printf("✅ ft_itoa\n");
+}
