b0df7b3ba7bd6a64dcaf6c62387b6194ce686245
Miguel Rios <miguelriosoliveira@gmail.com>
2023-09-24 00:39:36 +0200

    fix: Typecast and iteration limit on `ft_memchr`

--- cursus/00-libft/ft_memchr.c
+++ cursus/00-libft/ft_memchr.c
@@ -16,16 +16,16 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*str;
+	char	chr;
 
 	i = 0;
 	str = (char *)s;
-	while (i < n && str[i])
+	chr = c;
+	while (i < n)
 	{
-		if (str[i] == c)
+		if (str[i] == chr)
 			return (&str[i]);
 		i++;
 	}
-	if (c == '\0')
-		return (&str[i]);
 	return (NULL);
 }
--- cursus/00-libft/test/ft_memchr.c
+++ cursus/00-libft/test/ft_memchr.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include "../libft.h"
 
-int main() {
+int main()
+{
 	{
 		char *s = "great captain usopp";
 		char c = 'u';
@@ -12,7 +13,8 @@ int main() {
 		char *received = ft_memchr(s, c, n);
 
 		int passed = strcmp(expected, received) == 0;
-		if (!passed) {
+		if (!passed)
+		{
 			char *result = passed ? "✅" : "❌";
 			printf("%s ft_memchr(\"%s\")\n", result, s);
 			printf("expected: \"%s\"\n", expected);
@@ -29,7 +31,8 @@ int main() {
 		char *received = ft_memchr(s, c, n);
 
 		int passed = expected == received; // NULL
-		if (!passed) {
+		if (!passed)
+		{
 			char *result = passed ? "✅" : "❌";
 			printf("%s ft_memchr(\"%s\")\n", result, s);
 			printf("expected: \"%s\"\n", expected);
@@ -46,7 +49,80 @@ int main() {
 		char *received = ft_memchr(s, c, n);
 
 		int passed = expected == received; // NULL
-		if (!passed) {
+		if (!passed)
+		{
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_memchr(\"%s\")\n", result, s);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char s[] = {0, 1, 2, 3, 4, 5};
+		int c = 0;
+		int n = 0;
+
+		char *expected = memchr(s, c, n);
+		char *received = ft_memchr(s, c, n);
+
+		int passed = expected == received; // NULL
+		if (!passed)
+		{
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_memchr(\"%s\")\n", result, s);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char s[] = {0, 1, 2, 3, 4, 5};
+		int c = 0;
+		int n = 1;
+
+		char *expected = memchr(s, c, n);
+		char *received = ft_memchr(s, c, n);
+
+		int passed = expected == received; // NULL
+		if (!passed)
+		{
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_memchr(\"%s\")\n", result, s);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char s[] = {0, 1, 2, 3, 4, 5};
+		int c = 2 + 256;
+		int n = 3;
+
+		char *expected = memchr(s, c, n);
+		char *received = ft_memchr(s, c, n);
+
+		int passed = expected == received; // NULL
+		if (!passed)
+		{
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_memchr(\"%s\")\n", result, s);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char s[] = {-49, 49, 1, -1, 0, -2, 2};
+		int c = -1;
+		int n = 7;
+
+		char *expected = memchr(s, c, n);
+		char *received = ft_memchr(s, c, n);
+
+		int passed = expected == received; // NULL
+		if (!passed)
+		{
 			char *result = passed ? "✅" : "❌";
 			printf("%s ft_memchr(\"%s\")\n", result, s);
 			printf("expected: \"%s\"\n", expected);
