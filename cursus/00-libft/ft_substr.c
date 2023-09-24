ed6b474a9b060950a860519fe76a48086fd0d0b7
Miguel Rios <miguelriosoliveira@gmail.com>
2023-09-24 03:17:27 +0200

    fix: Typecast and memory usage on `ft_substr`

--- cursus/00-libft/ft_substr.c
+++ cursus/00-libft/ft_substr.c
@@ -12,20 +12,35 @@
 
 #include "libft.h"
 
+static int	min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
-	size_t	i;
+	int		i;
+	int		start_int;
+	int		length;
+	int		size;
 
 	if (start >= ft_strlen(s))
 		return (ft_calloc(1, sizeof(char)));
-	substr = ft_calloc(len + 1, sizeof(char));
+	start_int = start;
+	length = len;
+	if (length < 0)
+		length = ft_strlen(s);
+	size = min(start_int + length, ft_strlen(s)) - start_int;
+	substr = ft_calloc(size + 1, sizeof(char));
 	if (!substr)
 		return (NULL);
-	i = start;
-	while (i - start < len && s[i])
+	i = 0;
+	while (i < size)
 	{
-		substr[i - start] = s[i];
+		substr[i] = s[i + start_int];
 		i++;
 	}
 	return (substr);
--- cursus/00-libft/test/ft_substr.c
+++ cursus/00-libft/test/ft_substr.c
@@ -71,6 +71,57 @@ int main() {
 			return 1;
 		}
 	}
+	{
+		char *s = "hola";
+		unsigned int start = 0;
+		size_t len = -1;
+
+		char *expected = "hola";
+		char *received = ft_substr(s, start, len);
+
+		int passed = strcmp(expected, received) == 0;
+		if (!passed) {
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_substr(\"%s\", %d, %zu)\n", result, s, start, len);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char *s = "hola";
+		unsigned int start = 2;
+		size_t len = 0;
+
+		char *expected = "";
+		char *received = ft_substr(s, start, len);
+
+		int passed = strcmp(expected, received) == 0;
+		if (!passed) {
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_substr(\"%s\", %d, %zu)\n", result, s, start, len);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
+	{
+		char *s = "hola";
+		unsigned int start = 2;
+		size_t len = 3;
+
+		char *expected = "la";
+		char *received = ft_substr(s, start, len);
+
+		int passed = strcmp(expected, received) == 0;
+		if (!passed) {
+			char *result = passed ? "✅" : "❌";
+			printf("%s ft_substr(\"%s\", %d, %zu)\n", result, s, start, len);
+			printf("expected: \"%s\"\n", expected);
+			printf("received: \"%s\"\n", received);
+			return 1;
+		}
+	}
 
 	printf("✅ ft_substr\n");
 }
