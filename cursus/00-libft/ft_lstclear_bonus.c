b8c8ca1130e70b07032ff20056cb96e239855148
Miguel Rios <mrios-es@c1r9s4.42madrid.com>
2023-09-26 11:21:24 +0200

    feature: `ft_lstclear_bonus`

--- cursus/00-libft/ft_lstclear_bonus.c
+++ cursus/00-libft/ft_lstclear_bonus.c
@@ -12,4 +12,19 @@
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*curr;
+	t_list	*next;
+
+	if (!lst)
+		return ;
+	curr = *lst;
+	while (curr)
+	{
+		next = curr->next;
+		ft_lstdelone(curr, del);
+		curr = next;
+	}
+	*lst = NULL;
+}
