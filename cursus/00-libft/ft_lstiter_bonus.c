de564e8311c792f896ce47038640b6e63bade10f
Miguel Rios <mrios-es@c1r9s4.42madrid.com>
2023-09-26 11:56:37 +0200

    feature: `ft_lstiter_bonus`

--- cursus/00-libft/ft_lstiter_bonus.c
+++ cursus/00-libft/ft_lstiter_bonus.c
@@ -12,4 +12,14 @@
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*curr;
+
+	curr = lst;
+	while (curr)
+	{
+		f(curr->content);
+		curr = curr->next;
+	}
+}
