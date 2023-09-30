7f7ec23a94e0b3d73fee4452f0b3c1f76a0eece4
Miguel Rios <miguelriosoliveira@gmail.com>
2023-09-30 12:49:28 +0200

    chore: Removed unnecessary brackets

--- cursus/01-ft_printf/ft_printf.c
+++ cursus/01-ft_printf/ft_printf.c
@@ -81,37 +81,21 @@ int	ft_printf(const char *format, ...)
 			i++;
 			char_count--;
 			if (format[i] == 'c')
-			{
 				char_count += ft_putchar_fd(va_arg(args, int), 1);
-			}
 			else if (format[i] == 's')
-			{
 				char_count += ft_putstr_fd(va_arg(args, char *), 1);
-			}
 			else if (format[i] == 'p')
-			{
 				char_count += print_address(va_arg(args, void *));
-			}
 			else if (ft_strchr("di", format[i]))
-			{
 				char_count += ft_putnbr_fd(va_arg(args, int), 1);
-			}
 			else if (format[i] == 'u')
-			{
 				char_count += ft_putnbr_uint_fd(va_arg(args, unsigned int), 1);
-			}
 			else if (format[i] == 'x')
-			{
 				char_count += ft_putnbr_base(va_arg(args, int), HEX_BASE_LOWER);
-			}
 			else if (format[i] == 'X')
-			{
 				char_count += ft_putnbr_base(va_arg(args, int), HEX_BASE_UPPER);
-			}
 			else if (format[i] == '%')
-			{
 				char_count += ft_putchar_fd('%', 1);
-			}
 		}
 		else
 			ft_putchar_fd(format[i], 1);
