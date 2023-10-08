66b1bd09b5212e54cecd83e895b28b7dd39b6e51
Miguel Rios <mrios-es@c1r9s4.42madrid.com>
2023-10-08 17:01:23 +0200

    fix: strncmp readability

--- cursus/00-libft/ft_strncmp.c
+++ cursus/00-libft/ft_strncmp.c
@@ -12,8 +12,6 @@
 
 #include "libft.h"
 
-#include <stdio.h>
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t			i;
@@ -23,8 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	c1 = s1[0];
-	c2 = s2[0];
+	c1 = 1;
+	c2 = 1;
 	while (i < n && c1 && c2 && c1 == c2)
 	{
 		c1 = s1[i];
