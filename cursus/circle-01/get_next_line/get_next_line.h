e96b277d29a53b97210690c2caaaf37539a08f9c
Miguel Rios <mrios-es@c1r9s4.42madrid.com>
2023-11-18 21:42:58 +0100

    wip: Preparing to read next lines

--- cursus/circle-01/get_next_line/get_next_line.h
+++ cursus/circle-01/get_next_line/get_next_line.h
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 32
 #endif
 
 #ifndef GET_NEXT_LINE_H
--- cursus/circle-01/get_next_line/test/main.c
+++ cursus/circle-01/get_next_line/test/main.c
@@ -4,23 +4,45 @@
 #include "../get_next_line.h"
 
 int main(void) {
-	char *filename = "test/file.txt";
-
-	FILE *fp = fopen(filename, "r");
-    char *expected = NULL;
-    size_t len = 0;
-	getline(&expected, &len, fp);
-
-	int fd = open(filename, O_RDONLY);
-	char *received = get_next_line(fd);
-
-	int passed = strcmp(expected, received) == 0;
-	if (!passed) {
-		char *result = passed ? "✅" : "❌";
-		printf("%s get_next_line(\"%s\"): \"%s\"\n", result, filename, received);
-		printf("expected: \"%s\"\n", expected);
-		printf("received: \"%s\"\n", received);
-		return 1;
+	{
+		char *filename = "test/file.txt";
+		FILE *fp = fopen(filename, "r");
+		char *expected = NULL;
+		size_t len = 0;
+		int fd = open(filename, O_RDONLY);
+		int line_count = 0;
+		while (getline(&expected, &len, fp) > 0) {
+			line_count++;
+			char *received = get_next_line(fd);
+			int passed = strcmp(expected, received) == 0;
+			if (!passed) {
+				char *result = passed ? "✅" : "❌";
+				printf("%s get_next_line(\"%s\") on line %d: \"%s\"\n", result, filename, line_count, received);
+				printf("expected: \"%s\"\n", expected);
+				printf("received: \"%s\"\n", received);
+				return 1;
+			}
+		}
+	}
+	{
+		char *filename = "test/hp1.txt";
+		FILE *fp = fopen(filename, "r");
+		char *expected = NULL;
+		size_t len = 0;
+		int fd = open(filename, O_RDONLY);
+		int line_count = 0;
+		while (getline(&expected, &len, fp) > 0) {
+			line_count++;
+			char *received = get_next_line(fd);
+			int passed = strcmp(expected, received) == 0;
+			if (!passed) {
+				char *result = passed ? "✅" : "❌";
+				printf("%s get_next_line(\"%s\") on line %d: \"%s\"\n", result, filename, line_count, received);
+				printf("expected: \"%s\"\n", expected);
+				printf("received: \"%s\"\n", received);
+				return 1;
+			}
+		}
 	}
 
 	printf("✅ PASSED\n");
