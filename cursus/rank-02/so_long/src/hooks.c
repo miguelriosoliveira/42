8c43b970b5eacedc599ba7757ac56cffd52209a8
mrios-es <mrios-es@student.42madrid.com>
2024-09-28 18:42:38 +0200

    chore: Norm on `hooks.c`

--- cursus/rank-02/so_long/src/hooks.c
+++ cursus/rank-02/so_long/src/hooks.c
@@ -6,13 +6,41 @@
 /*   By: mrios-es <mrios-es@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 21:23:10 by mrios-es          #+#    #+#             */
-/*   Updated: 2024/09/28 16:41:39 by mrios-es         ###   ########.fr       */
+/*   Updated: 2024/09/28 18:34:30 by mrios-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	move(t_vars *vars, int direction)
+static void	try_move(t_vars *vars, t_sprite **sprite, int direction)
+{
+	if (direction == DIR_UP)
+	{
+		*sprite = &vars->sprites.player_up;
+		if (vars->map.content[vars->player.y - 1][vars->player.x] != MAP_WALL)
+			vars->player.y--;
+	}
+	if (direction == DIR_DOWN)
+	{
+		*sprite = &vars->sprites.player_down;
+		if (vars->map.content[vars->player.y + 1][vars->player.x] != MAP_WALL)
+			vars->player.y++;
+	}
+	if (direction == DIR_LEFT)
+	{
+		*sprite = &vars->sprites.player_left;
+		if (vars->map.content[vars->player.y][vars->player.x - 1] != MAP_WALL)
+			vars->player.x--;
+	}
+	if (direction == DIR_RIGHT)
+	{
+		*sprite = &vars->sprites.player_right;
+		if (vars->map.content[vars->player.y][vars->player.x + 1] != MAP_WALL)
+			vars->player.x++;
+	}
+}
+
+static void	move(t_vars *vars, int direction)
 {
 	t_sprite	*sprite;
 	int			x;
@@ -20,30 +48,8 @@ void	move(t_vars *vars, int direction)
 
 	x = vars->player.x;
 	y = vars->player.y;
-	if (direction == DIR_UP)
-	{
-		sprite = &vars->sprites.player_up;
-		if (vars->map.content[y - 1][x] != MAP_WALL)
-			vars->player.y--;
-	}
-	if (direction == DIR_DOWN)
-	{
-		sprite = &vars->sprites.player_down;
-		if (vars->map.content[y + 1][x] != MAP_WALL)
-			vars->player.y++;
-	}
-	if (direction == DIR_LEFT)
-	{
-		sprite = &vars->sprites.player_left;
-		if (vars->map.content[y][x - 1] != MAP_WALL)
-			vars->player.x--;
-	}
-	if (direction == DIR_RIGHT)
-	{
-		sprite = &vars->sprites.player_right;
-		if (vars->map.content[y][x + 1] != MAP_WALL)
-			vars->player.x++;
-	}
+	try_move(vars, &sprite, direction);
+	render(vars, sprite);
 	if (vars->player.x != x || vars->player.y != y)
 	{
 		vars->player.n_steps++;
@@ -61,10 +67,9 @@ void	move(t_vars *vars, int direction)
 		ft_printf("YOU WIN!\n");
 		on_destroy(vars);
 	}
-	render(vars, sprite);
 }
 
-int on_keypress(int keycode, t_vars *vars)
+int	on_keypress(int keycode, t_vars *vars)
 {
 	if (keycode == ESC)
 		on_destroy(vars);
@@ -79,11 +84,12 @@ int on_keypress(int keycode, t_vars *vars)
 	return (0);
 }
 
-int on_destroy(t_vars *vars)
+int	on_destroy(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
 	mlx_destroy_display(vars->mlx);
 	free(vars->mlx);
+	free_matrix(vars->map.content, vars->map.height);
 	exit(0);
 	return (0);
 }
