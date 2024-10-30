66e5972e253a287336cc536653db24e2a856a740
mrios-es <mrios-es@student.42madrid.com>
2024-10-30 20:45:31 +0100

    wip: Reading env vars

--- cursus/rank-02/pipex/src/pipex.c
+++ cursus/rank-02/pipex/src/pipex.c
@@ -6,14 +6,37 @@
 /*   By: mrios-es <mrios-es@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:04:12 by mrios-es          #+#    #+#             */
-/*   Updated: 2024/10/26 18:05:29 by mrios-es         ###   ########.fr       */
+/*   Updated: 2024/10/30 20:45:10 by mrios-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
-	ft_printf("hello %s\n", "world");
+	(void)env;
+	int		i;
+	char	*files[2];
+	char	*cmds[2];
+
+	if (argc != 5)
+		return (ft_printf("Bad arguments!\n$ ./pipex file1 cmd1 cmd2 file2\n"));
+
+	files[0] = argv[1];
+	cmds[0] = argv[2];
+	cmds[1] = argv[3];
+	files[1] = argv[4];
+	ft_printf("files[0]: \"%s\"\n", files[0]);
+	ft_printf(" cmds[0]: \"%s\"\n", cmds[0]);
+	ft_printf(" cmds[1]: \"%s\"\n", cmds[1]);
+	ft_printf("files[1]: \"%s\"\n", files[1]);
+
+	i = 0;
+	while (env[i])
+	{
+		ft_printf("[%2d] %s\n", i+1, env[i]);
+		i++;
+	}
+
 	return (0);
 }
