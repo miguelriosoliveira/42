cce4ca353787856b03628e4b7a933153956e2937
Miguel Rios <miguelriosoliveira@gmail.com>
2024-12-17 17:31:45 +0100

    feature: More test cases

--- cursus/rank-02/pipex/test/test.sh
+++ cursus/rank-02/pipex/test/test.sh
@@ -1,28 +1,32 @@
 ./pipex
 
-./pipex infile
+./pipex test/input
 
-./pipex infile "ls -l"
+./pipex test/input "ls -l"
 
-./pipex infile "ls -l" "wc -l"
+./pipex test/input "ls -l" "wc -l"
 
-./pipex test/infile "lssss -l" "wc -l" test/outfile1
+./pipex test/input "lssss -l" "wc -l" test/output_pipex
 
-./pipex test/non-existent-file "ls -l" "wc -l" test/outfile1
+./pipex test/non-existent-file "ls -l" "wc -l" test/output_pipex
 
-touch test/invalid_permission_read_infile \
-	&& chmod -r test/invalid_permission_read_infile \
-	&& ./pipex test/invalid_permission_read_infile "ls -l" "wc -l" test/outfile1 \
-	; rm test/invalid_permission_read_infile
+touch test/invalid_permission_read_input \
+	&& chmod -r test/invalid_permission_read_input \
+	&& ./pipex test/invalid_permission_read_input "ls -l" "wc -l" test/output_pipex \
+	; rm test/invalid_permission_read_input
 
-./pipex test/infile "ls -l" "wc -l"  test/outfile1 \
-   && < test/infile  ls -l | wc -l > test/outfile2 \
-   && diff -s test/outfile1 test/outfile2
+./pipex test/input "ls -l" "wc -l"  test/input_output_pipex \
+   && < test/input  ls -l | wc -l > test/input_output_shell \
+   && diff -s test/input_output_pipex test/input_output_shell
 
-./pipex test/lorem "cat" "wc -m"  test/outfile1 \
-   && < test/lorem  cat | wc -m > test/outfile2 \
-   && diff -s test/outfile1 test/outfile2
+./pipex test/lorem "cat" "wc -m"  test/lorem_output_pipex \
+   && < test/lorem  cat | wc -m > test/lorem_output_shell \
+   && diff -s test/lorem_output_pipex test/lorem_output_shell
 
-./pipex test/pokemon "cat" "sort"  test/outfile1 \
-   && < test/pokemon  cat | sort > test/outfile2 \
-   && diff -s test/outfile1 test/outfile2
+./pipex test/pokemon "cat" "sort"  test/pokemon_output_pipex \
+   && < test/pokemon  cat | sort > test/pokemon_output_shell \
+   && diff -s test/pokemon_output_pipex test/pokemon_output_shell
+
+./pipex test/names "cat" "sort -f"  test/names_output_pipex \
+   && < test/names  cat | sort -f > test/names_output_shell \
+   && diff -s test/names_output_pipex test/names_output_shell
