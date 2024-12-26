ac4c12a9b5062b5d043e2f2537f01df8e9854199
Miguel Rios <miguelriosoliveira@gmail.com>
2024-12-26 11:01:21 +0100

    refactor: Moves validation functions to their own file

--- cursus/rank-02/push_swap/src/push_swap.c
+++ cursus/rank-02/push_swap/src/push_swap.c
@@ -6,70 +6,11 @@
 /*   By: mrios-es <mrios-es@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 11:33:01 by mrios-es          #+#    #+#             */
-/*   Updated: 2024/12/24 15:31:37 by mrios-es         ###   ########.fr       */
+/*   Updated: 2024/12/26 10:57:17 by mrios-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	is_numeric(char *str)
-{
-	if (str == NULL || *str == '\0')
-		return (0);
-	if (*str == '+' || *str == '-')
-		str++;
-	if (*str == '\0')
-		return (0);
-	while (*str)
-	{
-		if (!ft_isdigit(*str))
-			return (0);
-		str++;
-	}
-	return (1);
-}
-
-int	is_present(char *str, char **words)
-{
-	int	i;
-	int	largest_len;
-
-	i = 0;
-	while (words[i])
-	{
-		largest_len = max(ft_strlen(str), ft_strlen(words[i]));
-		if (ft_strncmp(str, words[i], largest_len) == 0)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	validate_args(int argc, char **argv)
-{
-	char	**args;
-	int		i;
-	long	number;
-
-	if (argc < 2)
-		return (EXIT_SUCCESS);
-	args = argv + 1;
-	if (argc == 2)
-		args = ft_split(argv[1], ' ');
-	i = 0;
-	while (args[i])
-	{
-		if (!is_numeric(args[i]))
-			return (EXIT_FAILURE);
-		number = ft_atoi(args[i]);
-		if (number < INT_MIN || number > INT_MAX)
-			return (EXIT_FAILURE);
-		if (is_present(args[i], args + i + 1))
-			return (EXIT_FAILURE);
-		i++;
-	}
-	return (EXIT_SUCCESS);
-}
 
 int	main(int argc, char **argv)
 {
--- cursus/rank-02/push_swap/src/push_swap.h
+++ cursus/rank-02/push_swap/src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mrios-es <mrios-es@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 11:41:29 by mrios-es          #+#    #+#             */
-/*   Updated: 2024/12/24 11:43:02 by mrios-es         ###   ########.fr       */
+/*   Updated: 2024/12/26 10:57:46 by mrios-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+
+// validation.c
+int	validate_args(int argc, char **argv);
 
 #endif
